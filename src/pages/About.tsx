c38484a2be2d16ea709d20fbb06260dfbba4c337
Jose Martinez <joariel17@gmail.com>
2024-04-17 22:59:00 -0300

    Add pagina about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,18 @@
+import { Section } from '../components/Section'
+import img from '../assets/pets1.jpg'
+
 export const About = () => {
   return (
-    <div className='pt-14 dark:text-gray-50'>About</div>
+    <Section>
+        <article className='flex flex-col justify-center items-center w-full gap-5'>
+          <p className='dark:text-gray-50 md:w-[510px] text-pretty font-thin text-center max-md:text-base text-lg xl:text-xl max-sm:px-1'>
+            <span className='font-medium tracking-wide block'>¡Adopta una mascota y cambia su vida para siempre!</span>
+            <span className='block'>
+              Las mascotas son seres maravillosos que nos brindan amor incondicional y alegría en nuestras vidas. Si estás pensando en agregar un nuevo miembro peludo a tu familia, la adopción es una opción maravillosa y gratificante.
+            </span>
+          </p>
+          <img src={img} className='max-h-[300px] xl:max-h-[450px]' alt="img pets" />
+        </article>
+    </Section>
   )
 }
